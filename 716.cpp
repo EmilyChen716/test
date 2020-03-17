1e1c13e0511f5bc8c095e408c7b9d07c11ec6f1c
Emily <rose716225@gmail.com>
2020-03-17 08:43:45 +0800

    AB

---
+++ 716.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=0,b=1;
+	if (a>b)
+	{
+		cout<<"A"<<endl;
+	}
+	else if (b>a)
+	{
+		cout<<"B"<<endl;
+	}
+	else
+	{
+		cout<<"AB"<<endl;
+	}
+}
